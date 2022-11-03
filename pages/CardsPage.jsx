6c752ca27803b6fc7bbe874989bb6b905138430f
myroslav <myrkozar@gmail.com>
2022-11-03 14:02:52 +0200

    add: conect with db

--- pages/CardsPage.jsx
+++ pages/CardsPage.jsx
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 import PageCards from '../app/component/screens/PageCards/PageCards';
 
-
-const CardsPage = () => {
+const CardsPage = ({ unauthCardsArr }) => {
+  // console.log(unauthCardsArr);
   return (
     <div>
       <Head>
@@ -10,10 +10,24 @@ const CardsPage = () => {
         <meta name="description" content="Here are word cards" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <PageCards />
+      <PageCards unauthCardsArrProp={unauthCardsArr} />
     </div>
   );
 };
 
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch('http://localhost:3000/api/unauthCards');
+  const unauthCardsArr = await res.json();
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      unauthCardsArr,
+    },
+  };
+}
 
 export default CardsPage;
