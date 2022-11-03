18636b0176517485d608bf8df6e3457d90b3f0de
Maksimchik36 <Peltek1985@gmail.com>
2022-11-03 21:25:04 +0200

    add: cardsButtons

--- app/component/ui/Button.jsx
+++ app/component/ui/Button.jsx
@@ -7,10 +7,17 @@ const Button = ({
   idxCard,
   arrCards,
   cardIndex,
+  onOutlineNotificationClick,
+  isEventOutlineNotification,
+  onOutlineDeleteClick,
+  isEventOutlineDelete,
+  onMinusCircleClick,
+  isEventMinusCircle,
+  onOutlineSwapClick,
+  isEventOutlineSwap,
   children,
 }) => {
   const changeCardIdx = () => {
-    // console.log('cardIndexfirst', cardIndex);
     if (cardIndex + Number(action) >= 0 && action[0] === '-') {
       idxCard(prev => prev + Number(action));
     }
@@ -18,15 +25,40 @@ const Button = ({
       idxCard(prev => prev + Number(action));
     }
     return;
-  };
+  }
 
+  // кнопки на карточке
+  const actionByCardsButton = (e) => {
+    e.stopPropagation();
+    switch (true) {
+      case isEventOutlineNotification: onOutlineNotificationClick();
+        
+        break;
+      case isEventOutlineDelete: onOutlineDeleteClick();
+        
+        break;
+      case isEventMinusCircle: onMinusCircleClick();
+        
+        break;
+      case isEventOutlineSwap: onOutlineSwapClick();
+        
+        break;
+    
+      default: changeCardIdx();
+        break;
+    }
+
+  }
+     
   return (
     <button
       data-action={action}
       className={stylesProp}
       style={{ width: `${widthbtn}`, height: `${heightbtn}` }}
       type={type}
-      onClick={changeCardIdx}
+      // onClick={isEventMinusCircle ? onMinusCircleClick : changeCardIdx}
+       onClick={actionByCardsButton}
+
     >
       {children}
     </button>
